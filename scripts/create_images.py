41e502788a6e2d50208aaf4e6849b3503a6b828f
evila <emanuele.villa@cern.ch>
2024-02-05 18:19:55 +0100

    Improved and cleaned scripts

--- scripts/create_images.py
+++ scripts/create_images.py
@@ -28,25 +28,23 @@ parser = argparse.ArgumentParser(description='Tranforms Trigger Primitives to im
 parser.add_argument('-i', '--input-file',           type=str,                         help='Input file name')
 parser.add_argument('-o', '--output-path',          type=str,    default='./',        help='path to save the image')
 parser.add_argument('-n', '--n-tps',                type=int,    default=1,           help='number of tps to process')
-parser.add_argument('-t', '--ticks_limit',          type=int,    default=3,           help='closeness in ticks to group TPs')
-parser.add_argument('-c', '--channel_limit',        type=int,    default=1,           help='closeness in channels to group TPs')
+parser.add_argument('-t', '--ticks-limit',          type=int,    default=3,           help='closeness in ticks to group TPs')
+parser.add_argument('-c', '--channel-limit',        type=int,    default=1,           help='closeness in channels to group TPs')
 parser.add_argument('-m', '--min-tps',              type=int,    default=3,           help='minimum number of TPs to create a group and then an image')
 parser.add_argument('--which-detector',             type=str,    default="APA",       help='"APA", "CRP" or "50L"')
 parser.add_argument('--save-groups',                action='store_true',              help='write the groups to a textfile')
 parser.add_argument('--show',                       action='store_true',              help='show the image')
-# parser.add_argument('--save-ds',                  action='store_true',              help='save the dataset') # discuss if to keep it
 parser.add_argument('--fixed-size',                 action='store_true',              help='make the image size fixed')
-parser.add_argument('--img-width',                  type=int,    default=40,          help='width of the image')
-parser.add_argument('--img-height',                 type=int,    default=300,         help='height of the image')
-parser.add_argument('--x-margin',                   type=int,    default=5,           help='margin in x')
-parser.add_argument('--y-margin',                   type=int,    default=10,          help='margin in y')
-# parser.add_argument('--min-tps-to-create-img', type=int,    default=5,          help='minimum number of TPs to create an image')
+parser.add_argument('--img-width',                  type=int,    default=200,          help='width of the image, if fixed size')
+parser.add_argument('--img-height',                 type=int,    default=300,         help='height of the image, if fixed size')
+parser.add_argument('--x-margin',                   type=int,    default=2,           help='margin in x')
+parser.add_argument('--y-margin',                   type=int,    default=2,          help='margin in y')
+parser.add_argument('-v', '--verbose',              action='store_true',              help='verbose mode')
 
 args = parser.parse_args()
 input_file              = args.input_file
 output_path             = args.output_path
 show                    = args.show
-# save_ds               = args.save_ds
 n_tps                   = args.n_tps
 ticks_limit             = args.ticks_limit
 channel_limit           = args.channel_limit
@@ -58,7 +56,7 @@ width                   = args.img_width
 height                  = args.img_height
 x_margin                = args.x_margin
 y_margin                = args.y_margin
-# min_tps_to_create_img = args.min_tps_to_create_img
+verbose                 = args.verbose
 
 # recap of selected options
 print ("#############################################")
@@ -72,22 +70,31 @@ print(" - Minimum number of TPs to create a group: " + str(min_tps))
 print(" - Which detector: " + str(which_detector))
 print(" - Save groups: " + str(save_groups))
 print(" - Show image: " + str(show))
-# print(" - Save dataset: " + str(save_ds))
 print(" - Fixed size: " + str(fixed_size))
 print(" - Image width: " + str(width))
 print(" - Image height: " + str(height))
 print(" - X margin: " + str(x_margin))
 print(" - Y margin: " + str(y_margin))
-# print(" - Minimum number of TPs to create an image: " + str(min_tps_to_create_img))
+print(" - Verbose: " + str(verbose))
 print ("#############################################")
+print (" ")
 
 # reading with this function already orders by time_start
 all_TPs = save_tps_array(input_file, max_tps=n_tps) 
 
+if verbose:
+    print("TPs read from file: ", input_file)
+    print("Number of TPs: ", len(all_TPs))
+    print(" ")
+
 # create channel map to distinguish induction and collection
 channel_map = create_channel_map_array(which_detector=which_detector)
 
-groups = make_groups(all_TPs, channel_map, ticks_limit=ticks_limit, channel_limit=channel_limit, min_tps_to_group=min_tps)
+groups = make_groups(all_TPs, channel_map, 
+                     ticks_limit=ticks_limit, 
+                     channel_limit=channel_limit, 
+                     min_tps_to_group=min_tps)
+
 print("Number of groups: ", len(groups))
 
 # Check how many views (U, V, X) are in this sample
@@ -101,42 +108,46 @@ if not os.path.exists(output_path):
     os.makedirs(output_path)
     print ("Created output folder: " + output_path)
 
-print("Creating images...")
 for i, group in enumerate(groups):
-    save_image(np.array(group), channel_map, output_path=output_path, output_name= "track" + str(i), min_tps_to_create_img=min_tps, make_fixed_size=fixed_size, width=width, height=height, x_margin=x_margin, y_margin=y_margin, only_collection=False, show=show)
+    save_image(group, 
+               channel_map, 
+               output_path=output_path, 
+               output_name= "track" + str(i),
+               min_tps_to_create_img=min_tps, 
+               make_fixed_size=fixed_size, 
+               width=width, 
+               height=height, 
+               x_margin=x_margin,
+               y_margin=y_margin,
+               only_collection=False,
+               show=show)
 print("Done!")
 print(" ")
     
 if save_groups:
+    output_groupsFile = os.path.basename(input_file)
+    output_groupsFile = os.path.splitext(output_groupsFile)[0]
+    output_groupsFile += '_groups.txt'    
     print("Writing groups to text file...")
-    with open(output_path + 'groups.txt', 'w') as f:
+    with open(output_groupsFile, 'w') as f:
         for i, group in enumerate(groups):
-            f.write('Group' + str(i) + ':\n')
+            f.write('Group ' + str(i) + ':\n')
             for tp in group:
-                f.write()
-                this_tp = np.array([tp["time_start"], tp["time_over_threshold"], tp["time_peak"], tp["channel"], tp["adc_integral"], tp["adc_peak"], tp["detid"], tp["type"], tp["algorithm"], tp["version"], tp["flag"]])
+                # f.write(str(this_tp) + '\n')
+                this_tp = np.array([int(tp["time_start"]), 
+                                    int(tp["time_over_threshold"]),
+                                    int(tp["time_peak"]),
+                                    int(tp["channel"]), 
+                                    int(tp["adc_integral"]),
+                                    int(tp["adc_peak"]),
+                                    int(tp["detid"]), 
+                                    int(tp["type"]),
+                                    int(tp["algorithm"]),
+                                    int(tp["version"]),
+                                    int(tp["flag"])])
                 f.write (str(this_tp) + '\n')
                 # f.write(f"{tp["time_start"]} {tp[1]} {tp[2]} {tp[3]} {tp[4]} {tp[5]} {tp[6]} {tp[7]}\n")
                 # f.write(f"{tp[0]} {tp[1]} {tp[2]} {tp[3]} {tp[4]} {tp[5]} {tp[6]} {tp[7]}\n")
             f.write('\n')
     print("Done!")
     print(" ")
-
-# if save_ds:
-#     print("Creating dataset...")
-#     if not os.path.exists(output_path+'dataset/'):
-#         os.makedirs(output_path+'dataset/')
-#     dataset_img, dataset_label = tp2img.create_dataset(groups,  channel_map=channel_map, make_fixed_size=make_fixed_size, width=width, height=height, x_margin=x_margin, y_margin=y_margin, n_views=n_views)
-#     print("Dataset shape: ", dataset_img.shape)
-#     print("Dataset label shape: ", dataset_label.shape)
-
-#     # print how many entries are different from 0
-#     print("Number of non-zero entries: ", np.count_nonzero(dataset_img))
-
-#     np.save(output_path+'dataset/dataset_img.npy', dataset_img)
-#     np.save(output_path+'dataset/dataset_label.npy', dataset_label)
-
-#     print(np.unique(dataset_label,return_counts=True))
-#     print("Done!")
-# print('Done!')
-
--- scripts/plot_tp_properties.py
+++ scripts/plot_tp_properties.py
@@ -14,23 +14,22 @@ import argparse
 import sys
 sys.path.append("../python/")
 from properties_plotter import *
-from utils import * 
-
 
 # parse from command line the args.files and the number of tps to plot
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--files", nargs="+", help="files to read")
-parser.add_argument("-e", "--output-folder", type=str, help="output folder", default="./output/")
-parser.add_argument("-n", "--number-tps", type=int, help="number of tps to plot")
-parser.add_argument("-s", "--superimpose", action="store_true", help="superimpose plots")
-parser.add_argument("-a", "--all", action="store_true", help="plot all the TP variables")
-parser.add_argument("-t", "--time-peak", action="store_true", help="plot time peak")
-parser.add_argument("-o", "--time-over-threshold", action="store_true", help="plot time over threshold")
-parser.add_argument("-i", "--adc-integral", action="store_true", help="plot adc integral")
-parser.add_argument("-p", "--adc-peak", action="store_true", help="plot adc peak")
-parser.add_argument("-c", "--channel", action="store_true", help="plot channel")
-parser.add_argument("-d", "--detid", action="store_true", help="plot detid")
-parser.add_argument("--show", action="store_true", help="show plots", default=False)
+parser.add_argument("-f", "--files",                nargs="+",      help="files to read")
+parser.add_argument("-e", "--output-folder",        type=str,       help="output folder",   default="./")
+parser.add_argument("-n", "--number-tps",            type=int,    help="number of tps to plot")
+parser.add_argument("-s", "--superimpose",           action="store_true",    help="superimpose plots")
+parser.add_argument("-a", "--all",                   action="store_true",    help="plot all the TP variables")
+parser.add_argument("-t", "--time-peak",             action="store_true",    help="plot time peak")
+parser.add_argument("-o", "--time-over-threshold",   action="store_true",    help="plot time over threshold")
+parser.add_argument("-i", "--adc-integral",          action="store_true",    help="plot adc integral")
+parser.add_argument("-p", "--adc-peak",              action="store_true",    help="plot adc peak")
+parser.add_argument("-c", "--channel",               action="store_true",    help="plot channel")
+parser.add_argument("-d", "--detid",                 action="store_true",    help="plot detid")
+parser.add_argument("--show",                        action="store_true",    help="show plots", default=False)
+parser.add_argument("-v", "--verbose",               action="store_true",    help="verbose mode", default=False)
 args = parser.parse_args()
 
 if args.all:
@@ -42,50 +41,58 @@ if args.all:
     args.detid = True
 
 # print recap of options
-print ("#############################################")
-print ("Selected options:")
-print (" - Files: " + str(args.files))
-print (" - Number of TPs: " + str(args.number_tps))
-print (" - Superimpose: " + str(args.superimpose))
-print (" - All: " + str(args.all))
-print (" - Time peak: " + str(args.time_peak))
-print (" - Time over threshold: " + str(args.time_over_threshold))
-print (" - ADC integral: " + str(args.adc_integral))
-print (" - ADC peak: " + str(args.adc_peak))
-print (" - Channel: " + str(args.channel))
-print (" - Detid: " + str(args.detid))
-print (" - Show: " + str(args.show))
-print ("#############################################")
-    
+print("#############################################")
+print("Selected options:")
+print(" - Files: " + str(args.files))
+print(" - Number of TPs: " + str(args.number_tps))
+print(" - Superimpose: " + str(args.superimpose))
+print(" - All: " + str(args.all))
+print(" - Time peak: " + str(args.time_peak))
+print(" - Time over threshold: " + str(args.time_over_threshold))
+print(" - ADC integral: " + str(args.adc_integral))
+print(" - ADC peak: " + str(args.adc_peak))
+print(" - Channel: " + str(args.channel))
+print(" - Detid: " + str(args.detid))
+print(" - Show: " + str(args.show))
+print(" - Verbose: " + str(args.verbose))
+print("#############################################")
+
 # read the file(s) and create arrays of TPs, using the class in TriggerPrimitive.py
 # the code will deduce if the file is coming from offline or online data
 
 tps_lists = []
-for tpFile_path in  args.files:
-    
-    print ("Reading file: " + tpFile_path)
-    
+for tpFile_path in args.files:
+
+    print("Reading file: " + tpFile_path)
+
     this_tps_list = save_tps_array(tpFile_path, args.number_tps)
+    
+    if args.verbose:
+        print("TPs list: ", this_tps_list)
+    
     tps_lists.append(this_tps_list)
+    
 
 
-
+print("Plotting time peak...")
 if args.time_peak:
-    plotTimePeak(tps_lists, args.files, 
-                 superimpose=args.superimpose, 
-                 quantile=1, 
+    plotTimePeak(tps_lists, args.files,
+                 superimpose=args.superimpose,
+                 quantile=1,
                  y_min=None, y_max=None,
                  output_folder=args.output_folder,
-                 show=args.show) 
+                 show=args.show)
 
+print("Plotting time over threshold...")
 if args.time_over_threshold:
     plotTimeOverThreshold(tps_lists, args.files,
-                            superimpose=args.superimpose,
-                            quantile=1,
-                            y_min=None, y_max=None,
-                            output_folder=args.output_folder,
-                            show=args.show)
+                          superimpose=args.superimpose,
+                          quantile=1,
+                          y_min=None, y_max=None,
+                          output_folder=args.output_folder,
+                          show=args.show)
 
+print("Plotting ADC integral...")
 if args.adc_integral:
     plotADCIntegral(tps_lists, args.files,
                     superimpose=args.superimpose,
@@ -94,6 +101,7 @@ if args.adc_integral:
                     output_folder=args.output_folder,
                     show=args.show)
 
+print("Plotting ADC peak...")
 if args.adc_peak:
     plotADCPeak(tps_lists, args.files,
                 superimpose=args.superimpose,
@@ -102,6 +110,7 @@ if args.adc_peak:
                 output_folder=args.output_folder,
                 show=args.show)
 
+print("Plotting channel...")
 if args.channel:
     plotChannel(tps_lists, args.files,
                 superimpose=args.superimpose,
@@ -110,10 +119,13 @@ if args.channel:
                 output_folder=args.output_folder,
                 show=args.show)
             
-
+print ("Plotting detid...")
 if args.detid:
     plotDetId(tps_lists, args.files,
                 superimpose=args.superimpose,
                 y_min=None, y_max=None,
                 output_folder=args.output_folder,
                 show=args.show)
+
+print(" ")
+print ("Finished plotting! Products are in: " + args.output_folder + " and are named after the input file and variables plotted.")
